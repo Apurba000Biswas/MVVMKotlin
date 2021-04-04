3e59dce3da874db09b047103edbc7cbebfbb5485
apurba <apurba10024@gmail.com>
2021-04-05 00:48:11 +0600

    1.1 Updated Read Me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# MVVMKotlin
+# MVVM-Kotlin
+
+This project is created for practise purpose of Kotlin with MVVM pattern.
+
+### Acknowledgement :
+
+1. [Coding with mitch](https://codingwithmitch.com/courses/jetpack-compose-mvvm-for-beginners)
--- app/src/main/java/com/example/mvvmkotlin/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkotlin/MainActivity.kt
@@ -11,14 +11,3 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-
-/*
-# MVVM-Kotlin
-
-This project is created for practise purpose of Kotlin with MVVM pattern.
-
-### Acknowledgement :
-
-1. [Coding with mitch](https://codingwithmitch.com/courses/jetpack-compose-mvvm-for-beginners)
-
- */
