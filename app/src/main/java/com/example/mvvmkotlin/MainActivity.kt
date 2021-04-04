23a74f441ee15cfb196a2cd7abd4149b11bac25e
apurba <apurba10024@gmail.com>
2021-04-05 00:47:15 +0600

    Added All Files

---
+++ app/src/main/java/com/example/mvvmkotlin/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mvvmkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+/*
+# MVVM-Kotlin
+
+This project is created for practise purpose of Kotlin with MVVM pattern.
+
+### Acknowledgement :
+
+1. [Coding with mitch](https://codingwithmitch.com/courses/jetpack-compose-mvvm-for-beginners)
+
+ */
